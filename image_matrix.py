c5d282103ec74fe14a5f18645b9a21ee7d678a91
DrewTAJ <drew.t.a.johnson@gmail.com>
2017-12-28 17:00:25 -0500

    edited J

--- image_matrix.py
+++ image_matrix.py
@@ -166,14 +166,14 @@ def drawJ(minX, minY, maxY, width, height, colour):
         coords = [
                 (minX, minY),
                 (minX, minY + 4),
-                (minX + (width / 2) - 2, minY + 4),
-                (minX + (width / 2) - 2, maxY - 4),
+                (minX + (width / 2) - 1, minY + 4),
+                (minX + (width / 2) - 1, maxY - 4),
                 (minX + 4, maxY - 4),
                 (minX + 4, minY + (height / 2) - 2),
                 (minX, minY + (height / 2) - 2),
                 (minX, maxY),
-                (minX + (width / 2) + 2, maxY),
-                (minX + (width / 2) + 2, minY + 4),
+                (minX + (width / 2) + 3, maxY),
+                (minX + (width / 2) + 3, minY + 4),
                 (maxX, minY + 4),
                 (maxX, minY)
         ]
