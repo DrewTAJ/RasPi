4d4f939fccc04763b483c0f8474fb378f085d2f4
DrewTAJ <drew.t.a.johnson@gmail.com>
2017-12-29 14:21:51 -0500

    edited Q

--- image_matrix.py
+++ image_matrix.py
@@ -355,16 +355,23 @@ def drawQ(minX, minY, maxY, width, height, colour):
         drawByCoords(coords, colour)
 
         coords = [
+
                 (minX + 2, minY + 3),
                 (minX + 2, maxY - 7),
-                (maxX - 6, maxY - 7),
-
-
-                (maxX - 8, maxY - 10),
-                (maxX - 6, maxY - 10),
-
-                (maxX - 4, maxY - 10),
+                (maxX - 4, maxY - 7),
                 (maxX - 4, minY + 3)
+
+
+                # (minX + 2, minY + 3),
+                # (minX + 2, maxY - 7),
+                # (maxX - 6, maxY - 7),
+
+
+                # (maxX - 8, maxY - 10),
+                # (maxX - 6, maxY - 10),
+
+                # (maxX - 4, maxY - 10),
+                # (maxX - 4, minY + 3)
         ]
         drawByCoords(coords, colour)
 
